78c09f2764f4104f11d52b53219e25e138198a02
Eduard Coll Vila <eduard.collvila@altran.com>
2019-02-08 12:40:59 +0100

    FIX: Remove comments

--- src/main/java/hocklo/samples/graphql/resolvers/Query.java
+++ src/main/java/hocklo/samples/graphql/resolvers/Query.java
@@ -2,15 +2,10 @@ package hocklo.samples.graphql.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import hocklo.samples.graphql.entities.Car;
-import hocklo.samples.graphql.enums.Brand;
 import hocklo.samples.graphql.services.GarageService;
 import lombok.AllArgsConstructor;
-import org.apache.catalina.Store;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -23,30 +18,10 @@ public class Query implements GraphQLQueryResolver {
 
   public List<Car> cars() {
     return garageService.get();
-    /*List<Car> cars = new ArrayList<>();
-
-    Car car = new Car();
-    car.setId(UUID.randomUUID());
-    car.setName("Ford shelby gt500");
-    car.setLaunchAt(LocalDateTime.of(1967, Month.JANUARY.getValue(), 1, 0, 0));
-    car.setType(Brand.FORD);
-    car.setDiscontinued(true);
-
-    cars.add(car);
-
-    return cars;*/
   }
 
   public Optional<Car> car(UUID id) {
 
-//    Car car = new Car();
-//    car.setId(id);
-//    car.setName("Ford shelby gt500");
-//    car.setLaunchAt(LocalDateTime.of(1967, Month.JANUARY.getValue(), 1, 0, 0));
-//    car.setType(Brand.FORD);
-//    car.setDiscontinued(true);
-//
-//    return car;
     return garageService.get(id);
   }
 }
